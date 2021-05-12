c2820a801f0b2a2ad5a73a08058fb96bba0f527a
aidamoslehi <aida.moslehi@yahoo.com>
2021-05-12 21:40:13 +0430

    Add files via upload

---
+++ Dicee-Game/index.js
@@ -0,0 +1,22 @@
+//Get Random Number
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+//Change Dice Image
+document
+  .querySelectorAll("img")[0]
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+// Change title
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
