c704bf2b2575ada9c56936c8d0edcd9e367cbe31
aidamoslehi <aida.moslehi@yahoo.com>
2021-05-23 21:12:05 +0430

    Add files via upload

---
+++ Drum-Kit-With-jQuery/index.js
@@ -0,0 +1,74 @@
+// Adding Event Listeners to a Button
+// Detecting Button Press
+
+var numberOfDrumButtons = $(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  $(".drum").on("click", function () {
+    var buttonInnerHTML = $(this).text();
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// Detecting key Press
+
+$(document).on("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
+}
+
+// Adding Animation to Websites
+
+function buttonAnimation(currentKey) {
+  //currentKey is just a single character and we need to concatenate a full stop in addition to the currentKey
+  $("." + currentKey).addClass("pressed");
+
+  //We add this method that can remove animation after 100 MS
+  setTimeout(function () {
+    $("." + currentKey).removeClass("pressed");
+  }, 100);
+}
